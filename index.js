7b3254035b0d05c1452e7016955a9290b43ea567
unknown <ganatraarpita@gmail.com>
2021-07-20 10:00:22 +0530

    deploy changes

--- index.js
+++ index.js
@@ -32,8 +32,10 @@ mongoose.set("useFindAndModify", false);
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+}
 // Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+// app.get("*", function (req, resp) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
